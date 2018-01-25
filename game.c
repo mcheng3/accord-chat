d33b7c8cd6b465be46d1873b089ddacf64a31aaf
rchen10 <rchen10@stuy.edu>
2018-01-25 16:58:35 -0500

    Finish beta ver of hangman, and create react game

--- game.c
+++ game.c
@@ -2,12 +2,28 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define DELAY 30000
 
+void strreplace(char s[], char s1[],  char chr, char repl_chr)
+{
+     int i=0;
+     while(s[i]!='\0')
+     {
+           if(s[i]==chr)
+           {
+               s1[i]=repl_chr;
+           }  
+           i++; 
+     }
+     //printf("%s",s);
+}
 
 int hangman(char *phrase){
-	int i;
+  char *f = malloc(sizeof(char *));
+  int i;
+  strcpy(f, phrase);
 	int x = 0, y = 0;
 	int max_y = 0, max_x = 0;
 	int next_x = 0;
@@ -24,8 +40,16 @@ int hangman(char *phrase){
 		mvprintw(max_y/2-2, max_x/5-6+i, "=");
 	}
 	mvprintw(max_y/2-9, max_x/5, "|");
-	
-	while(1) {
+	char *length = malloc(sizeof(int));
+	sprintf(length, "%d", strlen(phrase));
+	char *blanks=malloc(strlen(phrase)); 
+	for(i=0; i<strlen(phrase); i++){
+	  strcat(blanks, "_");
+	}
+	//mvprintw(max_y/2, max_x/5+4, blanks);
+        // mvprintw(max_y/2, max_x/5+15, phrase);
+	int solved = 0;
+	while(moves_wrong<7 && !solved) {
 		
 		char ch;
 		ch = getch();	
@@ -33,28 +57,68 @@ int hangman(char *phrase){
 		strnew = &ch;
 		if (strcmp(strnew, str) != 0){
 			str = strnew;
-		}*/		
-		if(ch != ' ' && moves_wrong < 7){
+		}*/
+	  
+		strreplace(f, blanks, ch, ch);
+		if(ch != ' ' && moves_wrong < 7 && !strchr(phrase, ch)){
 			moves_wrong++;
-		}	
-		
+		}
+		if(!strchr(blanks, '_')){
+		  mvprintw(max_y/2+5, max_x/5+10, "You are a winner!");
+		   mvprintw(max_y/2, max_x/5+15, blanks);
+		  refresh();
+		  sleep(2);
+		  solved = 1;
+   
+		}
 		int man_y[] = {max_y/2-8,max_y/2-7,max_y/2-7,max_y/2-7,max_y/2-6,max_y/2-5,max_y/2-5};
 		int man_x[] = {max_x/5, max_x/5, max_x/5-1, max_x/5+1, max_x/5,max_x/5-1,max_x/5+1};
 		char *man_parts[] = {"O", "|", "\\", "/", "|", "/", "\\"};
 		for(i=0;i<moves_wrong;i++){
 			mvprintw(man_y[i],man_x[i],man_parts[i]);
 		}
-		
-	    mvprintw(max_y/2, max_x/5+5, phrase);	
+		 mvprintw(max_y/2, max_x/5+15, blanks);
+	   	
 		refresh();
 
 		usleep(DELAY);
 	}
+	if(moves_wrong ==7){
+	  mvprintw(max_y/2+5, max_x/5+10, "You are a loser!");
+	}
+	 refresh();
+	 sleep(2);
 }
 
-int main(int argc, char *argv[]) {
-	
+int react(){
+  int x = 0, y = 0;
+  int max_y = 0, max_x = 0;
+  	getmaxyx(stdscr, max_y, max_x);
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_RED);
+  wbkgd( stdscr ,COLOR_PAIR(1));
+  refresh();
+  srand(time(NULL));
+  int r = rand() % 10000000 + 1000000;
+  usleep(r);
+  init_pair(2, COLOR_WHITE, COLOR_GREEN);
+  wbkgd( stdscr ,COLOR_PAIR(2));
+  time_t start = time(NULL);
+  time_t end;
+  while(getch() != ' '){
+    usleep(10);
+    end = time(NULL);
 
+  }
+  mvprintw(max_y/2, max_x/2, "Took %6.3f seconds\n", difftime(end, start));
+  
+  refresh();
+  sleep(6);
+}
+
+
+int main(int argc, char *argv[]) {
+    
 	initscr();
 	noecho();
 	cbreak();
@@ -64,7 +128,7 @@ int main(int argc, char *argv[]) {
 
 	char ch;
 	char *str = malloc(sizeof(char));
-	hangman("potato");
-
+	//hangman("unpredictably");
+	react();
 	endwin();
 }
