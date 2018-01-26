4a2c19b7ad1c90d3efc3306702f970ec80ef43b2
edmond120 <edmondwong888@msn.com>
2018-01-26 03:53:24 -0500

    maked

--- chat_interface.c
+++ chat_interface.c
@@ -1,4 +1,5 @@
 #include "chat_interface.h"
+#include "networking.h"
 
 char * input_buffer;
 int input_buffer_used_length;
@@ -238,16 +239,30 @@ void refresh_all(){
 	wrefresh(status_bar);
 }
 
-int main(){
-	init();
-	refresh();
-	wrefresh(input_win);
-	wrefresh(output_win);
-	wrefresh(message_win);
-	wrefresh(status_bar);
+int main(int argc, char **argv) {
+
+	int server_socket;
+	char write_buffer[256];
+	char read_buffer[256];
+
+	if (argc == 2)
+		server_socket = client_setup( argv[1]);
+	else
+		server_socket = client_setup( CS16 );
+
+
+	set_display_pipe(server_socket);
 	while(1){
-		tick();
+		int retpoll = poll(poll_structs,1,0);
+		if(retpoll > 0){
+			if(poll_structs[0].revents & POLLIN){
+				read(server_socket, read_buffer, sizeof(read_buffer));
+				printf("%s", read_buffer);
+			}
+		}
+		printf(":");
+		fgets(write_buffer, sizeof(write_buffer), stdin);
+		//*strchr(write_buffer, '\n') = 0;
+		write(server_socket, write_buffer, sizeof(write_buffer));
 	}
-	endwin();
-	return 0;
 }
